bbada8e6729f47656037b96a06600687ed1de3a1
olivertian <791020605@qq.com>
2019-07-06 22:01:13 +0800

    准备动手改评论区了，comment model大换血

--- myblog/comment/forms.py
+++ myblog/comment/forms.py
@@ -9,7 +9,7 @@ class CommentForm(forms.ModelForm):
         label="昵称",
         max_length=50,
         widget=forms.widgets.Input(
-            attrs={"class": "form-control", "style": "width:60%"}
+            attrs={"class": "form-control", "style": "width:30%"}
         )
     )
 
@@ -17,23 +17,23 @@ class CommentForm(forms.ModelForm):
         label="Email",
         max_length=50,
         widget=forms.widgets.EmailInput(
-            attrs={"class": "form-control", "style": "width:60%"}
+            attrs={"class": "form-control", "style": "width:30%"}
         )
     )
 
-    website = forms.CharField(
-        label="网站",
-        max_length=100,
-        widget=forms.widgets.URLInput(
-            attrs={"class": "form-control", "style": "width:60%"}
-        )
-    )
+    # website = forms.CharField(
+    #     label="网站",
+    #     max_length=100,
+    #     widget=forms.widgets.URLInput(
+    #         attrs={"class": "form-control", "style": "width:60%"}
+    #     )
+    # )
 
     content = forms.CharField(
         label="内容",
         max_length=500,
         widget=forms.widgets.Textarea(
-            attrs={"rows": 6, "cols": 60, "class": "form-control"}
+            attrs={"rows": 6, "cols": 60, "class": "form-control", "style": "width: 60%"}
         )
     )
 
@@ -47,7 +47,7 @@ class CommentForm(forms.ModelForm):
 
     class Meta:
         model = Comment
-        fields = ["nickname", "email", "website", "content"]
+        fields = ["nickname", "email", "content"]
 
 
 
